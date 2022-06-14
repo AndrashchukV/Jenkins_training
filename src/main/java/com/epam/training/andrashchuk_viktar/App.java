ac5f1ed0d14737b54e47feb6b4402aa815029222
Viktor Androshchuk <vandrosc13@gmail.com>
2022-06-14 12:16:28 +0300

    Project files

---
+++ src/main/java/com/epam/training/andrashchuk_viktar/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.andrashchuk_viktar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
